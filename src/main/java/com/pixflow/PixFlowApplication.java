dc66baefb8bc3c888576d3bb39a9933d88d5f010
EtherealStar <1693604162@qq.com>
2026-06-27 13:47:04 +0800

    init: initialize PixFlow Spring Boot application
    - Setup Maven pom.xml with project dependencies
    - Created base PixFlowApplication Spring Boot entry point
    - Added initial application.yml configuration

---
+++ src/main/java/com/pixflow/PixFlowApplication.java
@@ -0,0 +1,11 @@
+package com.pixflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixFlowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PixFlowApplication.class, args);
+    }
+}
